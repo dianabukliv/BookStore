78f2b8aa27b8849b88ca5ad121a8533c5028bbfd
Olia Sereda <sereda745@gmail.com>
2024-11-08 19:00:55 +0200

    add author and discount banner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import MainBanner from "./components/MainBanner/MainBanner";
-import { NewBooks } from "./components/NewBooks/NewBooks";
+import AuthorBanner from './components/AuthorBanner/AuthorBanner';
+import MainBanner from './components/MainBanner/MainBanner';
+import { NewBooks } from './components/NewBooks/NewBooks';
+import DiscountBanner from './components/DiscountBanner/DiscountBanner';
 
 function App() {
   return (
     <div className="App">
-        <MainBanner/>
+      <MainBanner />
       <NewBooks />
-        
+      <AuthorBanner />
+      <DiscountBanner />
     </div>
   );
 }
