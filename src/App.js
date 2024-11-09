eefb98da4b57e09e1a85499d9e85fe5b5cf26c91
Anastasia M <morozanastasia2005@gmail.com>
2024-11-09 20:57:44 +0200

    ADD: header and footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+
 function App() {
   return (
     <div className="App">
-     
     </div>
   );
 }
