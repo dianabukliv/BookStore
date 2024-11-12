8f13505009c95723af0dc9e142a73ca64a3c5830
Olia Sereda <sereda745@gmail.com>
2024-11-12 14:01:27 +0200

    configuration fot automatical deploy

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+// import Header from './components/Header/Header';
+// import MainBanner from './components/MainBanner/MainBanner';
+// import { NewBooks } from './components/NewBooks/NewBooks';
+// import AuthorBanner from './components/AuthorBanner/AuthorBanner';
+// import DiscountBanner from './components/DiscountBanner/DiscountBanner';
+// import Footer from "./components/Footer/Footer";
 
 
 function App() {
   return (
     <div className="App">
+      {/* <Header />
+      <MainBanner />
+      <NewBooks/>
+      <AuthorBanner />
+      <DiscountBanner/>
+      <Footer/> */}
     </div>
   );
 }
